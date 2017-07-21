6cbdd91b7e1d49c677f5b48c893b4a249f077681
amerriman <amm434@gmail.com>
2017-07-20 22:01:25 -0600

    added style

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 var app = angular.module('githubSearch', [])
-  .controller('mainCtrl',['$scope', '$timeout', 'searchApi', function($scope, $timeout, searchApi ){
+  .controller('mainCtrl',['$scope', '$timeout', '$log', 'searchApi', function($scope, $timeout, $log, searchApi ){
     var vm = this;
     vm.term = null;
     vm.message = "You must enter a search term";
     vm.error = false;
-    vm.sort = 'score';
+    vm.sort = "score";
     vm.searchTerm = null;
+    vm.noItems = false;
+    vm.noItemMessage = "";
 
-      vm.setSort = function(sortChoice){
+    /**
+     * Function to set a sort preference
+     * @param sortChoice [string]
+     */
+    vm.setSort = function(sortChoice){
       if(!sortChoice){
         $log.debug('No sort choice');
         return
@@ -15,7 +21,24 @@ var app = angular.module('githubSearch', [])
       vm.sort = sortChoice
     };
 
+    /**
+     * Function to clear previously displayed items
+     */
+    function clearItems(){
+      vm.items = null;
+      vm.noItems = false;
+      vm.noItemMessage = "";
+    }
+
+    /**
+     * Function to set a sort preference
+     * @param term [string]
+     * @param sort [string]
+     */
     vm.findRepos = function(term, sort){
+
+      clearItems();
+
       if(!term){
         vm.searchTerm = null;
         vm.error = true;
@@ -24,15 +47,26 @@ var app = angular.module('githubSearch', [])
         }, 3000);
         return
       }
+
       vm.searchTerm = term;
       var params = {};
       params.term = term;
       params.sort = sort;
 
       searchApi.get(params).then(function(resp){
-        vm.items = resp;
+        if(resp.items.length > 0){
+          vm.items = resp.items;
+        } else {
+          vm.noItems = true;
+          vm.noItemMessage = "No repositories matched your search."
+        }
+      }).catch(function(err){
+        vm.noItems = true;
+        vm.noItemMessage = "Ooops.  Something went wrong.";
+        $log.error("findRepos", err);
       });
       vm.term = null;
     }
+
   }]);
 
--- js/services/api.js
+++ js/services/api.js
@@ -3,7 +3,7 @@ app.service('searchApi', ['$http', '$log', function($http, $log){
   var GHApi = {
     get: function(params){
       return $http.get('https://api.github.com/search/repositories?q=' + params.term + '&sort=' + params.sort).then(function(response){
-        return response.data.items;
+        return response.data;
       }).catch(function(err){
         $log.error("GHApi: get", err);
       });
